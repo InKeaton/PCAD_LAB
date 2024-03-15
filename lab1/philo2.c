29ec3d35ce7a03e263610c94159e1ba6563829f1
Neorya <51713192+Neorya@users.noreply.github.com>
2024-03-15 01:41:39 +0100

    Make philo1.c e philo2.c
    philo2.c è ancora da modificare

---
+++ lab1/philo2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////
+/*	
+ *	--VERSIONE DEL PROBLEMA DEI FILOSOFI SENZA DEADLOCK--
+*/
+////////////////////////////////////////////////////////////////////////////////////////////
+
+#define NUMPHILO 5
+
+pthread_mutex_t mutexThread[NUMPHILO] = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER};
+int eat[NUMPHILO];
+
+void* Filosofo(void* i) {
+	unsigned volatile int numPhi = *(int*) i;
+	for(int mangia = 0; mangia <NUMPHILO; mangia++) {
+		unsigned volatile int bSx, bDx = numPhi;
+		bSx = (numPhi-1+NUMPHILO)%NUMPHILO;
+		printf("%d , %d | Cerco di prendere la bacchetta sinistra (%d) \n", numPhi, mangia, bSx);
+		pthread_mutex_lock(&mutexThread[bSx]);
+			printf("%d | Prendo la bacchetta sinistra (%d) \n", numPhi, bSx);
+			printf("%d | Cerco di prendere la bacchetta destra (%d) \n", numPhi, bDx);
+			pthread_mutex_lock(&mutexThread[bDx]);
+				printf("%d | Prendo la bacchetta destra (%d) \n", numPhi, bDx);
+				printf("%d | Mangio....... :))\n", numPhi);
+				printf("%d | Finito di mangiare....... :))\n", numPhi);
+			pthread_mutex_unlock(&mutexThread[bDx]);
+		pthread_mutex_unlock(&mutexThread[bSx]);
+		printf("%d | Rilascio entrambe la bacchette :) \n", numPhi);
+	}
+
+}
+int main(int arg, char** args) {
+	pthread_t philo[NUMPHILO];
+	int ar[NUMPHILO];
+	for(unsigned int i = 0; i < NUMPHILO; i++) { ar[i] = i; }
+	for(unsigned int i = 0; i < NUMPHILO; i++) {
+		if(pthread_create(&philo[i], NULL, Filosofo,  &ar[i]) != 0) { return EXIT_FAILURE; 	}
+	}
+	for(int i = 0; i < NUMPHILO; i++) {
+		if(pthread_join(philo[i], NULL) != 0) {
+			return EXIT_FAILURE; 
+		}
+	}
+	printf("END :|\n");
+	return 0;
+}
