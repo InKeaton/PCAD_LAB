bce776abb30d7eed9a0450e5a6a36a8c720fe8af
Neorya <51713192+Neorya@users.noreply.github.com>
2024-03-15 16:26:59 +0100

    New Makefile && Barrier

--- lab1/barrier.c
+++ lab1/barrier.c
@@ -1,9 +1,48 @@
-#include "barrer.h"
+#include "barrier.h"
 
-int pthread_my_barrier_init {
+unsigned int pthread_my_barrier_init(my_barrier* mb, unsigned int v) {
+	if(v == 0) { return -1; }
+	pthread_mutex_init(&mb->lock, NULL);
+	mb->vinit = v;
+	mb->val 	= 0;
+	return 0;
+}
 
-};
+unsigned int pthread_my_barrier_wait(my_barrier* mb) {
+	pthread_mutex_lock(&mb->lock);	
+		mb->val++;
+		if(mb->val >= mb->vinit) { pthread_cond_broadcast(&mb->varcond); }
+		while(mb->val < mb->vinit) {
+			printf("Non siamo abbastanza :((, siamo .... : %d\n", mb->val);
+			pthread_cond_wait(&mb->varcond, &mb->lock);
+			sleep(2);
+		}
+	pthread_mutex_unlock(&mb->lock);	
+	return 0;
+}
 
-int pthread_my_barrier_wait {
+my_barrier m;	
 
-};
+void* prova() {
+	pthread_my_barrier_wait(&m);
+	printf("Ewwiwa :)))\n");
+}
+
+int main() {
+	pthread_t t1, t2, t3, t4, t5;
+	pthread_my_barrier_init(&m, 5);
+
+	pthread_create(&t1,NULL, prova, NULL);
+	pthread_create(&t2,NULL, prova, NULL);
+	pthread_create(&t3,NULL, prova, NULL);
+	pthread_create(&t4,NULL, prova, NULL);
+	pthread_create(&t5,NULL, prova, NULL);
+	
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
+	pthread_join(t4, NULL);
+	pthread_join(t5, NULL);
+
+	return 1;
+}
--- lab1/barrier.h
+++ lab1/barrier.h
@@ -1,11 +1,13 @@
 #include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
 typedef struct my_barrier {
     volatile unsigned int vinit ;
     volatile unsigned int val ;
     pthread_mutex_t lock ;
-
     pthread_cond_t varcond ;
 } my_barrier ;
 
-unsigned int pthread_my_barrier_init ( my_barrier ⁎mb , unsigned int v );
-unsigned int pthread_my_barrier_wait ( my_barrier ⁎mb );
+unsigned int pthread_my_barrier_init(my_barrier *mb , unsigned int v );
+unsigned int pthread_my_barrier_wait(my_barrier	*mb);
