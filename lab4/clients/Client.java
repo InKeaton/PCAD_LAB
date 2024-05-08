8cf92b5f1d694baf44fb05cf1e08c75e88931e04
Neorya <51713192+Neorya@users.noreply.github.com>
2024-05-08 12:20:44 +0200

    Update Client.java

--- lab4/clients/Client.java
+++ lab4/clients/Client.java
@@ -6,5 +6,20 @@
 //##########################################//  
 
 public class Client implements Actor {
-    
+    private static int client_num = 0;                                                                                                                                                      
+    private int client_id;
+    private Socket client_socket = null;
+
+    Client() {
+        Socket socket=new Socket("localhost",4242);
+        BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter pw=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        pw.println("Blabla");
+        pw.flush();
+        String mess=br.readLine();
+        System.out.println("Message from the server:"+mess);
+        pw.close();
+        br.close();
+        socket.close();
+    }
 }
