c2cd6cf1e4c38c757b3d4d28fcd53f85fa69ae80
Neorya <51713192+Neorya@users.noreply.github.com>
2024-05-08 12:24:29 +0200

    Add files via upload

--- lab4/server/MainServer.java
+++ lab4/server/MainServer.java
@@ -1,3 +1,11 @@
-public class MainServer {
-    
+import java.io.*;
+import java.net.*;
+
+class MainServer {
+	
+	public static void main(String[] args) {
+		Server s = new Server();
+
+	}
 }
+	
--- lab4/server/Server.java
+++ lab4/server/Server.java
@@ -1,12 +1,30 @@
-//##########################################// 
-// # SERVER                                 //
-//##########################################// 
-// + An object containing a FIFO structure. //
-// + Waits for TCP requests from Producers  //
-//   and Consumers to add or remove         //
-//   elements from the structure            //
-//##########################################//  
+import java.io.*;
+import java.net.*;
 
-public class Server implements Actor {
-    
+public class Server {
+	private ServerSocket server;
+	private final int default_port = 6969;
+
+	Server() 				 { this.ServerRun(this.default_port); }
+	Server(int port) { this.ServerRun(port); }
+
+	private void ServerRun(int run_port) {
+		try {
+			this.server = new ServerSocket(run_port);
+			while(true) {
+				Socket socket = this.server.accept();
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter pw=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+				String mess=br.readLine();
+				System.out.println("Receive message:" + mess);
+				pw.println("ECHO "+mess);
+				pw.flush();
+				br.close();
+				pw.close();
+				socket.close();
+			}
+		} catch(Exception e) {
+		
+		}
+	}
 }
