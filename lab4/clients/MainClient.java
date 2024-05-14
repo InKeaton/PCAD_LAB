da5c3f8ec8443a92ab665234d12ecc161ae36d23
Neorya <51713192+Neorya@users.noreply.github.com>
2024-05-15 01:23:34 +0200

    Update MainClient.java

--- lab4/clients/MainClient.java
+++ lab4/clients/MainClient.java
@@ -1,6 +1,14 @@
+import java.util.*;
+
 public class MainClient {
 	
 	public static void main(String[] args) {
-		new Client("ccc");
+		try{
+			Thread t = new Thread(new Consumer(6969));
+			t.start();
+			t.join();
+		} catch(Exception e) {
+			System.out.println("Errore nel lancio del thread: " + e.getMessage());
+		}
 	}
 }
