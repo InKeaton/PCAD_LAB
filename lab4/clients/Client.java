7e0fc24df59448a5da5fdb11fa4f1fe4c7287900
Neorya <51713192+Neorya@users.noreply.github.com>
2024-05-15 01:20:09 +0200

    Adjust Client

--- lab4/clients/Client.java
+++ lab4/clients/Client.java
@@ -14,11 +14,12 @@ public class Client implements Actor {
 	private static int client_num = 0;
 	
 	private int client_id;
-	private Socket client_socket = null;
-	
+	private Socket client_socket  = null;
+	private PrintWriter pw 			  = null;
+	private BufferedReader br     = null;
 	private String[] snd_string;
 	private String[] rcv_string;
-	private int 	 snd_msg_index = 0;
+	private int 	 snd_msg_index  = 0;
 	private int 	 rcv_msg_index  = 0;
 
 	private int server_port;
@@ -29,49 +30,53 @@ public class Client implements Actor {
 		this.client_id = client_num++;
 		this.snd_string = snd_string;
 		this.rcv_string = rcv_string;
+		try {
+			this.client_socket = new Socket("localhost", this.server_port);
+			this.pw = new PrintWriter(new OutputStreamWriter(this.client_socket.getOutputStream()));
+			this.br = new BufferedReader(new InputStreamReader(this.client_socket.getInputStream()));
+		} catch(Exception e) {}
 		System.out.println("Creato il nuovo client numero: " + this.client_id + " manderà le richieste al server alla porta: " + this.server_port);
 	}
 
 	public void Snd() {
 		try {
-			PrintWriter pw = new PrintWriter(new OutputStreamWriter(this.client_socket.getOutputStream()));
+			System.out.println("Client: " + this.client_id + " send");
 			pw.println(this.snd_string[this.snd_msg_index]);
 			pw.flush();
-			pw.close();
+			System.out.println("inviato il seguente msg: " + this.snd_string[this.snd_msg_index]);
 		} catch (Exception e) {}
 	}
 
 
 	public void Rcv() {
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(this.client_socket.getInputStream()));
 			String rcv;
-			do {
-				rcv = br.readLine();
-			} while(rcv != this.rcv_string[this.rcv_msg_index]);
-			br.close();
-		} catch (Exception e) {}
+			System.out.println("Client: " + this.client_id + " recv");
+			rcv = br.readLine();
+			System.out.println("Ricevuto msg: " + rcv);
+		} catch (Exception e) {
+			System.out.println("Client RCV Exception: " + e.getMessage());
+		}
 	}
 
 	public void Client_Life() {
 		try {	
-			this.client_socket = new Socket("localhost", this.server_port);
-			while( this.rcv_msg_index  != (this.rcv_string.length - 1) || 
-				   this.snd_msg_index  != (this.snd_string.length - 1) ){
-				
-				if(this.rcv_msg_index != (this.rcv_string.length - 1))  { 
+			while(this.snd_msg_index < this.snd_string.length && this.rcv_msg_index < this.rcv_string.length) {
+				if(this.snd_msg_index < this.snd_string.length) {
 					this.Snd(); 
 					this.snd_msg_index++;
 				}
-
-				if(this.snd_msg_index != (this.snd_string.length - 1))  { 
+				if(this.rcv_msg_index < this.rcv_string.length) {
 					this.Rcv();  
 					this.rcv_msg_index++;
 				}
 			}
 			this.client_socket.close();
 		}	 
-		catch (Exception e) {}
+		catch (Exception e) {
+			System.out.println("Presa eccezione: " + e.getMessage());
+
+		}
 	}
 }
 
